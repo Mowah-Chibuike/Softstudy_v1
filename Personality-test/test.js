033d1b2d312f75b89eae470fba45604bfb389ca8
Mowah-Chibuike <104087576+Mowah-Chibuike@users.noreply.github.com>
2022-07-19 15:23:28 +0100

    Updating the files

--- Personality-test/test.js
+++ Personality-test/test.js
@@ -1,3 +1,4 @@
+const Main = document.getElementById("main");
 const radios = document.querySelectorAll("input[type='radio']");
 const set1 = document.getElementById("set1");
 const set2 = document.getElementById("set2");
@@ -30,7 +31,61 @@ const disable = () => {
   }
 };
 
-Main.addEventListener("load", disable());
+const userState = () => {
+  console.log(createLink("Sign Up", "Softstudy-sign-up/sign-up.html"));
+  console.log(createPTag("Sign Out", "sign-out"));
+
+  if (loggedIn === null) {
+    signUp.innerText = "";
+    signUp.append(createLink("Sign Up", "../Softstudy-sign-up/sign-up.html"));
+  } else {
+    signIn.style.visibility = "hidden";
+    signUp.innerText = "";
+    signUp.append(createPTag("Sign Out", "sign-out"));
+    signUp.addEventListener("click", () => {
+      const Token = sessionStorage.getItem("token");
+      fetch("https://Softstudy.herokuapp.com/api/learners/me/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + Token,
+        },
+      })
+        .then((response) => response.json())
+        .then((info) => {
+          const { status } = info;
+          if (status === "success") {
+            sessionStorage.clear();
+            authText.innerText = "You have logged out succesfully";
+            authText.style.color = "green";
+            setTimeout(showMsg, 0);
+            setTimeout(hideMsg, 3000);
+            setTimeout(() => {
+              window.location.replace("./index.html");
+            }, 0);
+          } else {
+            authText.innerText = "Unable to log out";
+            authText.style.color = "red";
+            setTimeout(showMsg, 0);
+            setTimeout(hideMsg, 3000);
+          }
+        })
+        .catch(() => {
+          authText.innerText = "Something went wrong";
+          authText.style.color = "red";
+          setTimeout(showMsg, 0);
+          setTimeout(hideMsg, 3000);
+        });
+    });
+  }
+};
+
+const loadFunc = () => {
+  disable();
+  userState();
+};
+
+Main.addEventListener("load", loadFunc());
 
 Form.addEventListener("submit", function (e) {
   e.preventDefault();
